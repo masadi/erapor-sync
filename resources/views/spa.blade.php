9609bd79e18eeb343dad34f840879c0e4ecb7b74
masadi <masadi.com@gmail.com>
2020-09-24 13:42:16 +0700

    update

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -14,7 +14,7 @@ $config = [
 
   <title>{{ config('app.name') }}</title>
 
-  <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset(mix('dist/css/app.css')) }}">
 </head>
 <body>
   <div id="app"></div>
@@ -25,6 +25,6 @@ $config = [
   </script>
 
   {{-- Load the application scripts --}}
-  <script src="{{ mix('dist/js/app.js') }}"></script>
+  <script src="{{ asset(mix('dist/js/app.js')) }}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,7 @@ require('laravel-mix-versionhash')
 mix
   .js('resources/js/app.js', 'public/dist/js')
   .sass('resources/sass/app.scss', 'public/dist/css')
-  .versionHash()
+  .version()
   .disableNotifications()
 
 if (mix.inProduction()) {
